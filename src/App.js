39f2533bf06d7f5f90bf211f49a99ad8b0f39954
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-02-01 21:50:01 +0530

    finally completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, React } from 'react';
+import { Navbar, Header, Main, Footer, Reservations, OrderOnline } from './components';
+import {Routes, Route, useLocation  } from "react-router-dom";
+import { headerData, reservationData, orderOnline } from './constants';
+import DataContext from './DataContext';
+import ConfirmedBooking from './components/Reservations/ConfirmedBooking';
 
 function App() {
+// use state to set data
+// use effect to get path
+  const location = useLocation();
+  const [path, setPath] = useState(location.pathname);
+
+  // set path name on route change
+  useEffect(()=> {
+    setPath(location.pathname);
+  }, [location.pathname]);
+
+
+    // Send data to header to based on route
+    let data = {};
+    if(path === '/' || path === '/Little-Lemon') {
+
+      data = headerData;
+
+    } else if (path === '/Reservations') {
+
+      data = reservationData;
+
+    } else if (path === '/OrderOnline') {
+
+      data = orderOnline;
+
+    }
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      
+          <Navbar />
+            <DataContext.Provider value={data}>
+                <Routes>
+                  <Route path='/Little-Lemon' element ={ 
+                    <>
+
+                      <Header />
+                      <Main />
+
+
+                    </>
+                  } />                  
+                  <Route path='/' element ={ 
+                    <>
+
+                      <Header />
+                      <Main />
+
+
+                    </>
+                  } />
+                  <Route path='/Reservations' element={
+                    <>
+
+                      <Header />
+                      <Reservations />
+
+                        
+                    </>
+                  } />
+                  <Route path='/OrderOnline' element={
+                    <>
+                      <Header />
+                      <OrderOnline />  
+                    </>
+                  } />                  
+
+                  {/* Path for booking confirmation */}
+                  <Route path='/ConfirmedBooking' element={<ConfirmedBooking/>}/>
+
+                </Routes>
+            </DataContext.Provider>
+          <Footer />
+
+      </>
   );
 }
 
